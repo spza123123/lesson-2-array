b53e6f5fd439ef8f6566f7e12a6838b549ab9037
Zakhar <info@zspirin.ru>
2024-02-10 19:09:47 +0300

    task2

---
+++ tsk2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // System.Console.WriteLine(arr[i]);
+    // System.Console.WriteLine(" ");
+    System.Console.WriteLine($"{arr[i]} ");
+    i++;
+}
